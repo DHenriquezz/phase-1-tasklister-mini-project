1d56b9e36965d3dd87292c9e4617ae60edcee463
Douglas Flores <Dflores5df@icloud.com>
2023-04-18 15:08:18 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    addTodo(e.target.querySelector('#new-task-description').value)
+    form.reset()
+  })
+
+   function addTodo(todo){
+    let li = document.createElement('li')
+    li.textContent = ` ${todo}`
+    document.querySelector('#tasks').appendChild(li)
+    li.style.color = "red"
+    li.style.fontFamily = 'Verdana'
+    let btn = document.createElement('button')
+    btn.textContent = 'x'
+    btn.addEventListener('click', deleteTask)
+    li.appendChild(btn)
+
+
+   }
+  
+   function deleteTask (e) {
+    e.target.parentNode.remove()
+   }
+
+  
+   
+  
+
 });
+
